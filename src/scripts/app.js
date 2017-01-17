909d1daf3d449ec0cf40d79072655b08c32aaace
jsherman <jacob@jbsherman.com>
2017-01-17 16:42:35 -0500

    map working but resultsArray undefined in RestaurantsViewModel

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,107 @@
 /**
  * Created by jsherman on 1/16/17.
  */
-/*global Backbone */
 var app = app || {};
 
 (function () {
     'use strict';
+    app.map;
+    app.resultsArray;
 
+
+    app.initMap = function() {
+        var styles = [
+            {
+                featureType: 'water',
+                stylers: [
+                    {color: '#19a0d8'}
+                ]
+            }, {
+                featureType: 'administrative',
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    {color: '#ffffff'},
+                    {weight: 6}
+                ]
+            }, {
+                featureType: 'administrative',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {color: '#e85113'}
+                ]
+            }, {
+                featureType: 'road.highway',
+                elementType: 'geometry.stroke',
+                stylers: [
+                    {color: '#efe9e4'},
+                    {lightness: -40}
+                ]
+            }, {
+                featureType: 'transit.station',
+                stylers: [
+                    {weight: 9},
+                    {hue: '#e85113'}
+                ]
+            }, {
+                featureType: 'road.highway',
+                elementType: 'labels.icon',
+                stylers: [
+                    {visibility: 'off'}
+                ]
+            }, {
+                featureType: 'water',
+                elementType: 'labels.text.stroke',
+                stylers: [
+                    {lightness: 100}
+                ]
+            }, {
+                featureType: 'water',
+                elementType: 'labels.text.fill',
+                stylers: [
+                    {lightness: -100}
+                ]
+            }, {
+                featureType: 'poi',
+                elementType: 'geometry',
+                stylers: [
+                    {visibility: 'on'},
+                    {color: '#f0e4d3'}
+                ]
+            }, {
+                featureType: 'road.highway',
+                elementType: 'geometry.fill',
+                stylers: [
+                    {color: '#efe9e4'},
+                    {lightness: -25}
+                ]
+            }
+        ];
+
+        var mapEle = document.getElementById('mapDiv');
+        var neighborhood =  {  // Jackson Heights MTA Train station lat ln
+            "lat" : 40.7466891,
+            "lng" : -73.8908579
+        };
+        app.map = new google.maps.Map(mapEle, {
+            center: neighborhood,
+            zoom: 16,
+            styles: styles,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            navigationControl: true,
+            navigationControlOptions: {
+                style: google.maps.NavigationControlStyle.SMALL
+            }
+        });
+        // based on google map place search api example
+        // https://developers.google.com/maps/documentation/javascript/examples/place-search
+        // infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(app.map);
+        service.nearbySearch({
+            location: neighborhood,
+            radius: 500,
+            type: ['restaurant']
+        }, app.callback);
+    };
     app.callback = function(results, status) {
         if (status === google.maps.places.PlacesServiceStatus.OK) {
             app.resultsArray = results;
@@ -52,16 +147,6 @@ var app = app || {};
         this.name =ko.observable(restaurantObj.name);
         this.placeId =ko.observable(restaurantObj.placeId);
     };
-            // geometry : {},
-            // icon : "",
-            // id : "",
-            // name : "",
-            // opening_hours : {},
-            // photos : [],
-            // place_id : "",
-            // price_level : null,
-            // rating : null,
-            // types : []
 
     // our main view model
     function RestaurantsViewModel() {
